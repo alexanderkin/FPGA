9a0d037d4285d7a42bf25cd69f29a57d407b489a
alexanderkin <211560569@qq.com>
2021-12-18 19:35:22 +0800

    3_breath_led

--- 3_breath_led/3_breath_led.srcs/sources_1/new/breath_led.v
+++ 3_breath_led/3_breath_led.srcs/sources_1/new/breath_led.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2021/12/17 19:57:38
-// Design Name: 
-// Module Name: breath_led
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module breath_led(
     input              clk  ,
     input              rst_n,
